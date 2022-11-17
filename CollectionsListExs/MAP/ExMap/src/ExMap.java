7fb42fcba003357108686c41328d81d087a346d5
Enzo Arthur Martins da Silva <43550853+Enzo11ea11@users.noreply.github.com>
2022-11-17 19:59:46 -0300

    Add files via upload

---
+++ CollectionsListExs/MAP/ExMap/src/ExMap.java
@@ -0,0 +1,79 @@
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class ExMap {
+    public static void main(String[] args) {
+        System.out.println("Crie um dicionário que relacione os modelos em seus respectivos consumos");
+        Map<String, Double> carrosPopulares = new HashMap<String, Double>() {
+            {
+                put("gol", 14.4);
+                put("uno", 15.4);
+                put("focus", 7.4);
+                put("palio", 12.4);
+            }
+        };
+        System.out.println(carrosPopulares);
+
+        System.out.println("Substitua o consumo do gol para 15.2");
+        carrosPopulares.put("gol", 15.2);
+        System.out.println(carrosPopulares);
+
+        System.out.println("Confira se tucson está no dicionário: " + carrosPopulares.containsKey("tucson"));
+        System.out.println("Confira se uno está no dicionário: " + carrosPopulares.containsKey("uno"));
+
+        System.out.println("Retorne o valor de consumo do uno: " + carrosPopulares.get("uno"));
+
+        System.out.println("Exiba os modelos dos carros: ");
+        Set<String> modelos = carrosPopulares.keySet();
+        System.out.println(modelos);
+
+        System.out.println("Exiba os consumos dos carros: ");
+        Collection<Double> consumo = carrosPopulares.values();
+        System.out.println(consumo);
+
+        System.out.println("Exiba o modelo mais econômico e seu consumo: ");
+        Double consumoEconomico = Collections.max(carrosPopulares.values());
+        Set<Map.Entry<String, Double>> entries = carrosPopulares.entrySet();
+        String modeloMaisEficiente = "";
+        for (Map.Entry<String, Double> entry : entries) {
+            if (entry.getValue().equals(consumoEconomico)) {
+                modeloMaisEficiente = entry.getKey();
+            }
+        }
+        System.out.println(modeloMaisEficiente);
+
+        System.out.println("Exiba o modelo menos econômico e seu consumo: ");
+        Double consumoMenosEficiente = Collections.min(carrosPopulares.values());
+        String modeloMenosEficiente = "";
+        for (Map.Entry<String, Double> entry : carrosPopulares.entrySet()) {
+            if (entry.getValue().equals(consumoMenosEficiente)) {
+                modeloMenosEficiente = entry.getKey();
+                System.out.println(modeloMenosEficiente + " - " + consumoMenosEficiente);
+            }
+        }
+
+        System.out.println("Exiba a soma dos consumos: ");
+        Iterator<Double> iterator = carrosPopulares.values().iterator();
+        Double soma = 0d;
+        while (iterator.hasNext()) {
+            soma += iterator.next();
+        }
+        System.out.println(soma);
+
+        System.out.println("Exiba a media dos consumos: " + (soma / (carrosPopulares.size())));
+
+        System.out.println("Remova os carros com sonsumo igual a 12.4");
+        Iterator<Double> iterator1 = carrosPopulares.values().iterator();
+        while (iterator1.hasNext()) {
+            if (iterator1.next().equals(12.4)) {
+                iterator1.remove();
+            }
+        }
+        System.out.println(carrosPopulares);
+
+    }
+}
